ad338c473fea2c31e1e86dbd579bd4b776af36d2
hayyantechonza <hayyan.techonza@gmail.com>
2024-07-12 11:24:00 +0500

    Complete site

--- js/script.js
+++ js/script.js
@@ -13,6 +13,38 @@ $(function() {
       prevEl: '.swiper-button-prev',
     },
   });
+  var trendingSlider = new Swiper('.trending_slider', {
+    loop: true,
+    speed: 800,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    slidesPerView: 1,
+    spaceBetween: 20,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+      375: {
+        slidesPerView: 1,
+      },
+      625: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+      1200: {
+        slidesPerView: 4,
+      }
+    }
+  });
 
   var popularStoresSlider = new Swiper(".popular_stores_slider", {
     loop: true,
@@ -37,4 +69,49 @@ $(function() {
     }
   });
 
+  var forms = $('.needs-validation');
+
+  forms.each(function() {
+    $(this).on('submit', function(event) {
+      event.preventDefault()
+      if (this.checkValidity() === false) {
+        event.stopPropagation()
+      }
+
+      $(this).addClass('was-validated');
+    })
+  });
+
+  setupScrollToTopButton();
+
+  $('.search_form input').on('input', function() {
+    var query = $(this).val().trim();
+    if (query) {
+      $(this).parents('.search_form').find("#search-results").show();
+    } else {
+      $(this).parents('.search_form').find("#search-results").hide();
+    }
+  });
+
 });
+
+
+
+function setupScrollToTopButton() {
+  var scrollToTopBtn = $('<button id="scrollToTopBtn" class="btn"><i class="ph ph-arrow-up"></i></button>');
+  scrollToTopBtn.hide();
+
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 20) {
+      scrollToTopBtn.fadeIn(100);
+    } else {
+      scrollToTopBtn.fadeOut(100);
+    }
+  });
+
+  scrollToTopBtn.click(function() {
+    $('html, body').animate({ scrollTop: 0 }, 50);
+  });
+
+  $('body').append(scrollToTopBtn);
+}
